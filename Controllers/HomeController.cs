6eb4218fd2ac04e988da5fbfb010ef3c066fe323
Rodolfo Fadino <rodolfo.fadino@gmail.com>
2022-06-02 21:09:12 -0300

    primeiro encontro

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace vitat_web.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+        {
+			//acesso ao banco de dados
+			var hora = DateTime.Now.ToString();
+
+			ViewData["horario"] = hora;
+
+			ViewBag.HoraDoBrasil = hora;
+
+
+			ViewData["horario"] = hora;
+			ViewBag.horario = hora;
+			ViewData["horario"] = "cucharro";
+
+
+			var pessoa = new Pessoa();
+			pessoa.Nome = "Rebeca";
+			pessoa.Idade = 25;
+
+			var homeViewModel = new HomeViewModel()
+			{ Pessoa = pessoa, Hora = hora };
+			
+			return View(homeViewModel);
+		}
+
+
+		public string ListaDeCategorias(string category)
+		{
+
+			return "Lista de " +category;
+		}
+	}
+
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+    }
+
+
+	public class HomeViewModel
+    {
+        public Pessoa Pessoa { get; set; }
+        public string Hora { get; set; }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+//builder.Services.AddControllers();
+
+var app = builder.Build();
+
+
+
+app.UseRouting();
+
+
+app.MapControllerRoute(
+    name: "rotax",
+    //pattern: "{controller=Home}/{action=Index}/{id?}",
+    pattern: "categoria/{category}/{id?}",
+    defaults: new { Controller = "Home", Action = "ListaDeCategorias" }
+    );
+
+app.MapControllerRoute(
+
+
+    name: "padrao",
+    //pattern: "{controller=Home}/{action=Index}/{id?}",
+    pattern: "{controller}/{action}/{id?}",
+    defaults: new { Controller = "Home", Action = "Index"}
+    );
+
+
+app.Run();
+
+
+//app.MapGet("/", () =>
+
+//"Olá Vitat"
+//);
+
+
+//app.MapGet("/admin", (string username) =>
+
+//"Olá Admin Vitat"+ username
+//);
+
+
+
+
